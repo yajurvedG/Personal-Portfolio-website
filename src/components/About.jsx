36a8638f5d7d66267462e5c71a11572cfd303569
yajurvedG <22M1867@iitb.ac.in>
2023-08-21 11:39:02 +0530

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
                     <div className='max-w-[1000px] w-full grid sm:grid-cols gap-8 px-4'>
 
                         <div className='sm:text-right  text-4xl font-bold'>
-                            <p>Hi,I'm Lakhvir,nice to meet you,Please take a look around.</p>
+                            <p>Hi,I'm Yajurved gupta,nice to meet you,Please take a look around.</p>
                         </div>
                         <div>
                             <p>I am passionate about building excellent software that improves the lives of those around me.I specilaise in creating software for clients ranging fro individals and small businesses all the way to large enterprise corporations.What would you do if you had a software expert available at your fingertips? </p>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-pink-600 '>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'> Lakhvir Singh</h1>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'> Yajurved gupta</h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'> I'm Full Stack Developer</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px] '>
             I'm a full-stack developer specialising in building digital experiences.Currently,I'm focused on building responsive full stack web application
